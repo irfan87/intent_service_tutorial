e7c604960accd1c9208de789b30c2f8a13f26830
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-06-25 14:22:18 +0800

    can used intent service (jobs service) for downloading file from the internet

--- app/src/main/java/com/irfanshukri203/intentservicetutorial/MainActivity.kt
+++ app/src/main/java/com/irfanshukri203/intentservicetutorial/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.irfanshukri203.intentservicetutorial
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnStartService.setOnClickListener {
+            Intent(this, MyIntentService::class.java).also {
+                startService(it)
+
+                tvServiceInfo.text = "Service running"
+            }
+        }
+
+        btnStopService.setOnClickListener {
+            MyIntentService.stopService()
+
+            tvServiceInfo.text = "Service stop"
+        }
     }
 }
